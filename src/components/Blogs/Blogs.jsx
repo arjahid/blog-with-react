8079baf4a7d7c4820ef6d5d0ae9a172e04a2f8d1
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-30 22:06:37 +0600

    added new functionality

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,32 +1,26 @@
 import { useEffect, useState } from "react";
 import Blog from "../Blog/Blog";
 
+const Blogs = ({ handleAddBookmark }) => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch("blog.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
 
-const Blogs = ({handleAddBookmark}) => {
-    const [blogs, setBlogs] = useState([])
-    useEffect(()=>{
-        fetch('blog.json')
-        .then(res => res.json())
-        .then(data => setBlogs(data)
-        )
-    },[])
-    
-    return (
-        <div className="md:w-2/3">
-            <h1 className="text-5xl">Blogs:{blogs.length}</h1>
-            {
-                blogs.map(blog=><Blog
-                     key={blog.id}
-                     blog={blog}
-                     handleAddBookmark={handleAddBookmark}
-                     >
-
-                           
-                        
-                      </Blog>)
-            }
-        </div>
-    );
+  return (
+    <div className="md:w-2/3">
+      <h1 className="text-5xl">Blogs:{blogs.length}</h1>
+      {blogs.map((blog) => (
+        <Blog
+          key={blog.id}
+          blog={blog}
+          handleAddBookmark={handleAddBookmark}
+        ></Blog>
+      ))}
+    </div>
+  );
 };
 
 export default Blogs;
